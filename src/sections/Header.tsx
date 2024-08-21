2fccfd0de4c345743fdaa5c65681cd80cb3b7828
Haseeb Ullah <ihaseebullah.8@gmail.com>
2024-08-21 22:24:27 +0500

    Added Hero Section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,12 @@
 export const Header = () => {
-  return <div>Header Section</div>;
+  return (
+    <div className="flex justify-center w-full items-center fixed top-3 z-10">
+      <nav className="flex gap-1 p-0.5 border border-white/15 bg-white/10 rounded-full backdrop-blur">
+        <a href="#" className="nav-item hover:bg-white/10 hover:text-white transition duration-300">Home</a>
+        <a href="#" className="nav-item hover:bg-white/10 hover:text-white transition duration-300">Projects</a>
+        <a href="#" className="nav-item hover:bg-white/10 hover:text-white transition duration-300">About</a>
+        <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-black">Contact</a>
+      </nav>
+    </div>
+  )
 };
