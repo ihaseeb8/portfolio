ed90557bb1cdda0c3685be193798505f746f8044
Haseeb Ullah <ihaseebullah.8@gmail.com>
2024-08-23 07:53:06 +0500

    Added all Pages

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,34 @@
+const words = [
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "Responsive",
+  "User Friendly",
+  "Usable",
+  "Reliable"
+]
+
+import StartIcone from '@/assets/icons/star.svg'
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <section className='py-16 -rotate-3 -mx-1 lg:py:24 overflow-x-clip'>
+      <div className='bg-gradient-to-r from-emerald-300 to-sky-400'>
+        <div className='flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_70%,transparent)]'>
+        
+        <div className='flex gap-4 flex-none py-3'>
+          {words.map( word => (
+            <div key={word} className='inline-flex gap-4 py-3'>
+              <span className='text-gray-900 uppercase text-sm font-extrabold'>{word}</span>
+              <StartIcone className='size-6 text-gray-900 -rotate-12' />
+            </div>
+          ))}
+        </div>
+
+        </div>
+      </div>
+    </section>
+  );
 };
