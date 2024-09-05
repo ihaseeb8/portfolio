6416d6731e55c1264f3804bce924c085b7b10d1b
ihaseeb8 <ihaseebullah.8@example.com>
2024-09-05 16:17:29 +0500

    Added about section

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,7 +8,7 @@ const Card = ({
     children,
   } : PropsWithChildren<{className?: string }>) => {
   return (
-    <div className={twMerge("bg-gray-800 overflow-hidden rounded-3xl relative z-0 after:z-10 after:content-[''] after:absolute after:inset-0 after:outline after:-outline-offset-2 after:pointer-events-none after:rounded-3xl after:outline-white/20 p-6", className)}>
+    <div className={twMerge("bg-gray-800 overflow-hidden rounded-3xl relative z-0 after:z-10 after:content-[''] after:absolute after:inset-0 after:outline after:-outline-offset-2 after:pointer-events-none after:rounded-3xl after:outline-white/20", className)}>
         <div className="absolute inset-0 -z-10 opacity-5" style={{backgroundImage: `url(${grainImage.src})`}}></div>
         {children}
     </div>
