890ce798052f75c2c9ea8d455a4bebf1146d413e
ihaseeb8 <ihaseebullah.8@example.com>
2024-09-06 16:50:40 +0500

    fixed some bugs

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -9,9 +9,9 @@ export const ContactSection = () => {
         <div className="absolute inset-0 opacity-5 -z-10" style={{backgroundImage: `url(${grainImage.src})`}}></div>
         <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
           <div>
-            <h2 className='font-serif text-2xl md:text-3xl'>Let's create something amazing together</h2>
+            <h2 className='font-serif text-2xl md:text-3xl'>{"Let's create something amazing together"}</h2>
             <p className='text-sm md:text-base mt-2'>
-              Ready to bring your next project to life? Let's connect and discuss how I can help you achieve your goals.
+              {"Ready to bring your next project to life? Let's connect and discuss how I can help you achieve your goals."}
             </p>
           </div>
           <div>
